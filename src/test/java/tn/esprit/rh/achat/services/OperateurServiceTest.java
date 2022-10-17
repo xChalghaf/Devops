96807c7532020768ea607f860b815da6252918d7
Elmoatazbelleh <47725323+Elmoatazbelleh@users.noreply.github.com>
2022-10-17 04:53:23 +0100

    NexusTry4

--- src/test/java/tn/esprit/rh/achat/services/OperateurServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/OperateurServiceTest.java
@@ -3,23 +3,23 @@ package tn.esprit.rh.achat.services;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import tn.esprit.rh.achat.entities.Stock;
-import tn.esprit.rh.achat.services.IStockService;
+import tn.esprit.rh.achat.entities.Operateur;
+import tn.esprit.rh.achat.services.IOperateurService;
 
 import java.util.List;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class StockServiceTest {
+public class OperateurServiceTest {
 
     @Autowired
-    IStockService ps;
+    IOperateurService ps;
 
     @Test
     @Order(1)
     public void testRetrieveAllUsers() {
-        List<Stock> listStocks = ps.retrieveAllStocks();
-        Assertions.assertEquals(0, listStocks.size());
+        List<Operateur> listOperateurs = ps.retrieveAllOperateurs();
+        Assertions.assertEquals(0, listOperateurs.size());
     }
 
 
